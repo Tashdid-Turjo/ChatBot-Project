e0b2534512fd11cd4d96f6cf46ef1adab3eaa293
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-02-05 16:40:03 +0600

    Added App component's code into chatbot-project/src/App.jsx file. Here, you will see some mistakes are showing by ESLint extension.

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -3,33 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+// TODO: Changed its code to my App component's code. Here, you will see some mistakes are showing by ESLint extension.
+function App() {
+  // Will convert chatMessages variable into State, thus it will then update the HTML & will show the updated values in the webpage. Using React.useState(), if we update its inside data, it will also update the HTML. React.useState() returns an array.
+  // Did Array Destructuring:
+  const [chatMessages, setChatMessages] = React.useState([{             // useState gives us two values. First value is the current data which is chatMessages. Second is setChatMessages, which is a function {Updater function} to update this data.
+    message: 'Welcome to the chatbot project! Send a message using the textbox below.',
+    id: 'id1'
+  }]);
+
+  // const chatMessages = array[0];                  // array[0] is current data which is first value of useState.
+  // const setChatMessages = array[1];               // array[1] is the second value which is a function to update this data. This updater function will update the HTML.
+  // Will use JS to convert this upper code's data into these components.
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="app-container">
+        <ChatMessages 
+          chatMessages={chatMessages}
+        />
+        {/* Here, By App Component -> these two child component's props are being send to their own individual. And inside ChatInput & ChatMessages component- they are receiving their props as i wrote there inside their parameter. Thus, props naming convention must be exact same. */}
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+    </div>
+  );
 }
 
 export default App
