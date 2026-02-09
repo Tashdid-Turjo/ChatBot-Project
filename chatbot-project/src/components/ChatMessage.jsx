61855ca8ae597344f066cb4a371cc6fd38f26524
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-02-09 18:04:16 +0600

    Doing some stuff.

--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,9 +1,25 @@
+import dayjs from 'dayjs'
 import RobotProfileImage from '../assets/robot.png'                    // RobotProfileImage can be any name & this name shouldn't contain curly braces.
 import UserProfileImage from '../assets/user.png'
 import './ChatMessage.css'
 
 
 export function ChatMessage({ message, sender }) {
+  const [saveTime, setSaveTime] = useState([]);
+
+  function sendMessage(text) {
+    // Implementing timestamp for each & every message
+    const time = dayjs().valueOf();     // To get the current time
+  
+    const newMessage = {
+      id: crypto.randomUUID(),
+      text,
+      time
+    };
+
+    setSaveTime([...saveTime, setSaveTime]);
+  }
+
   return (
     <div
       className={
@@ -16,7 +32,12 @@ export function ChatMessage({ message, sender }) {
         <img src={RobotProfileImage} className="chat-message-profile" />
       )}
       <div className="chat-message-text">
-        {message}
+        <div className="chat-message-content">
+          {message}
+        </div>
+        <div className="chat-message-time">
+          {dayjs(time).format('h:mma')}
+        </div>
       </div>
       {sender === 'user' && (
         <img src={UserProfileImage} className="chat-message-profile" />
