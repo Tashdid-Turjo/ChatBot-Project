ce62afc34088ef84cd70bc1572ca085c3cee5cdf
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-02-26 14:52:40 +0600

    React Router Setup
    1. Imported BrowserRouter. 2. In main.jsx file, added <BrowserRouter>.

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'        // Here, didn't included " ./ " bcz, this means, it's going to load it from node_modules inside react-router.
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
