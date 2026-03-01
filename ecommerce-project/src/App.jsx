ec90c53ef22e145218482dacc2599744ebdf4372
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-03-01 10:22:37 +0600

    1. Updates the links to match the router. 2. Moved OrdersPage,TrackingPage to React & added that Route(path,element) in App.jsx file.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router'
 import './App.css'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckOutPage'
+import { Routes, Route } from 'react-router'
+import { HomePage }      from './pages/HomePage'
+import { CheckoutPage }  from './pages/CheckOutPage'
+import { OrdersPage }    from './pages/OrdersPage'
+import { TrackingPage }  from './pages/TrackingPage'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index           element={<HomePage />}     />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders"   element={<OrdersPage />}       />
+      <Route path="tracking" element={<TrackingPage />}     />
     </Routes>
   )
 }
