c364e32ee46651565dc1f3f333aaa7f43b52b2c7
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-03-08 12:28:48 +0600

    Added CSS file for NotFoundPage.

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,11 +1,14 @@
 import { Header } from '../components/Header';
+import './NotFoundPage.css';
 import { Link } from 'react-router';
 
 export function NotFoundPage() {
   return (
     <>
+        <title>404 Page Not Found</title>
+        <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
         <Header />
-        <div>
+        <div className="not-found-message">
             <h1>Page not found</h1>
             <p>The page you are looking for does not exist.</p>
             <Link to="/">Go back to Home</Link>
