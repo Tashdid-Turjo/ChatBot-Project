423d104cfa563122b86b0814dabbb2d0047434ee
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-03-10 09:46:56 +0600

    Fetch product data from Backend.
    Now fetching/getting product's data from backend. We used- 1. built-in function fetch, 2. added the url of the backend, 3. then used .then() to wait for the response & convert it to JSON. 4. Logged the fetched product data in the console using console.log(data).

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,7 +6,9 @@ import { products } from "../../starting-code(imported-from-github-repo)/data/pr
 export function HomePage() {
   fetch('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response);
+      response.json().then((data) => {
+        console.log(data);
+      });
     })
 
   return (
